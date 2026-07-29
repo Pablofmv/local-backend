aa31ff16fecd9fe7a117c5c0959e751cd025467a
Pablo Mendoza <pablo_fmv@hotmail.com>
2026-07-29 12:33:17 -0400

    Complete Axum Fundamental and Error Handling

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,6 +1,7 @@
 use axum::{
-    Json,
-    extract::Path,
+    extract::{Path, Json},
+    http::StatusCode,
+    response::IntoResponse,
 };
 
 use crate::models::{CreateQuestion, Question};
@@ -13,8 +14,12 @@ pub async fn list_questions() -> &'static str {
     "Listing all questions"
 }
 
-pub async fn get_question(Path(id): Path<u64>) -> String {
-    format!("Show question {id}")
+pub async fn get_question(Path(id): Path<u64>) -> Result<String, StatusCode> {
+    if id == 1 {
+        Ok(format!("Showing question {}", id))
+    } else {
+        Err(StatusCode::NOT_FOUND)
+    }
 }
 
 pub async fn create_question(
@@ -30,8 +35,19 @@ pub async fn create_question(
     Json(question)
 }
 
-pub async fn update_question(Path(id): Path<u64>) -> String {
-    format!("Question {id} updated")
+pub async fn update_question(
+    Path(id): Path<u64>, 
+    Json(payload): Json<CreateQuestion>
+    ) -> Json<Question> {
+    
+    let question = Question {
+        id,
+        title: payload.title,
+        body: payload.body,
+        category: payload.category,
+    };
+
+    Json(question)
 }
 
 pub async fn delete_question(Path(id): Path<u64>) -> String {
